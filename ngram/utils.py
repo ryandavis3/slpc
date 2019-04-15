5458b96253f7de479b95bef86ae834f79906c558
davisr137 <davisr137@gmail.com>
2019-04-15 15:45:34 -0700

    utils

--- ngram/utils.py
+++ ngram/utils.py
@@ -2,7 +2,7 @@
 
 import re
 
-TRANSCRIPT_PATH = "slpc/data/transcript.txt"
+TRANSCRIPT_PATH = "data/transcript.txt"
 
 def read_transcript_text(filename=TRANSCRIPT_PATH):
     """
@@ -45,4 +45,44 @@ def remove_extraenous_phrases(words):
         list of str
     """
     pattern = '[\[\<](.*?)[\]\>]'
-    return [word for word in words if re.search(pattern, word) is None]
+    words = [word for word in words if re.search(pattern, word) is None]
+    words = [word for word in words if word != '.']
+    return words
+
+def build_ngram_dict(text, n=2):
+    """
+    Build nested dictionary representing ngrams in text.
+
+    Args:
+        text (list of str): Lines of text.
+        n (int): Number of words per n-gram.
+
+    Returns:
+        dict: Nested keys represent subsequent words
+            in an n-gram. Leaf values represent the count of the
+            n-gram in the text.
+    """
+    ngram_dict = dict()
+    for line in text:
+        line = remove_leading_code(line)
+        words = line.split()
+        words = remove_extraenous_phrases(words)
+        words = ['<s>'] + words + ['</s>']
+        ngrams = [words[i:i+n] for i in range(len(words)-n)]
+        for ngram in ngrams:
+            dict_level = ngram_dict
+            for i in range(n):
+                word = ngram[i]
+                if i < n-1:
+                    if word not in dict_level:
+                        dict_level[word] = dict()
+                    dict_level = dict_level[word]
+                else:
+                    if word not in dict_level:
+                        dict_level[word] = 0
+                    dict_level[word] += 1
+    return ngram_dict
+                
+
+
+    
